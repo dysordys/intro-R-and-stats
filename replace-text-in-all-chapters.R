39fa43cd289b4d14c61b89368e961b56d46bcaad
Gyorgy Barabas <dysordys@gmail.com>
2024-02-26 22:56:16 +0100

    More global changes (xlab and ylab to labs); started revising chapter 7 to work with the snails dataset instead of iris

--- replace-text-in-all-chapters.R
+++ replace-text-in-all-chapters.R
@@ -2,10 +2,11 @@ library(tidyverse)
 
 tibble(file = Sys.glob("*.qmd")) |>
   mutate(content = map(file, read_lines)) |>
-  mutate(content = map(content,
-                       \(x) str_replace_all(x, "summarise", "summarize"))) |>
-  mutate(content = walk2(content, file, write_lines))
+  mutate(hasContent = map_lgl(content, \(x) any(str_detect(x, "(x|y)lab")))) |>
+  filter(hasContent)
 
 tibble(file = Sys.glob("*.qmd")) |>
   mutate(content = map(file, read_lines)) |>
-  mutate(hasContent = map_lgl(content, \(x) any(str_detect(x, "autoplot"))))
+  mutate(content = map(content,
+                       \(x) str_replace_all(x, "summarise", "summarize"))) |>
+  mutate(content = walk2(content, file, write_lines))
