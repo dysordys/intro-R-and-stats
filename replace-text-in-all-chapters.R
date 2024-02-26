80375681de9e3dc3df8d635ac1e78a395e981769
Gyorgy Barabas <dysordys@gmail.com>
2024-02-26 22:14:00 +0100

    Fixes to conventions throughout the whole text (summarize instead of summarise; color instead of colour)

--- replace-text-in-all-chapters.R
+++ replace-text-in-all-chapters.R
@@ -3,5 +3,9 @@ library(tidyverse)
 tibble(file = Sys.glob("*.qmd")) |>
   mutate(content = map(file, read_lines)) |>
   mutate(content = map(content,
-                       \(x) str_replace_all(x, "data-with-R/", "intro-R-and-stats/"))) |>
+                       \(x) str_replace_all(x, "summarise", "summarize"))) |>
   mutate(content = walk2(content, file, write_lines))
+
+tibble(file = Sys.glob("*.qmd")) |>
+  mutate(content = map(file, read_lines)) |>
+  mutate(hasContent = map_lgl(content, \(x) any(str_detect(x, "autoplot"))))
